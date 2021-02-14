d63df0a01cfa25241c61967e76baa4b5225eed98
hamad-11 <zhmamapapa@gmail.com>
2021-02-14 15:14:52 +0900

    終わり

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    config.i18n.default_locale = :ja
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.generators do |g|
       g.stylesheets false
@@ -17,10 +15,7 @@ module ChatSpace
       g.helper false
       g.test_framework false
     end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
